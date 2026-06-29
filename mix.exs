cd12fa5fecea43d0e7fa300b8d09b23fcc6347f2
Manh Van Vu <vuvanmanh123@gmail.com>
2026-06-29 16:10:46 +0700

    fix bug: Codegen generates incorrect docs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AshScylla.MixProject do
   def project do
     [
       app: :ash_scylla,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
